43fa8f37cf9fa7967cf633910ef09033d6312292
Liu-Amy <aliu66@gatech.edu>
2016-04-24 01:54:26 -0400

    more sql updates

--- Phase3SQLStatements.sql
+++ Phase3SQLStatements.sql
@@ -38,7 +38,7 @@ WHERE Email = %s
 // Status: Works
 UPDATE Customers
 SET Customers.Student= "yes"
-WHERE Customers.Username = %s AND Customers.Email LIKE '%@%.edu%'
+WHERE Customers.Username = %s AND %s LIKE '%@%.edu'
 
 // Figure 5: View train schedule
 // Status: Works
@@ -53,8 +53,7 @@ SELECT CONCAT(StationName," (", Location, ")") as dropdownChoices, StationName
 FROM Stations
 
 // Figure 7: Select departure
-// Account for multiple legs of a train
-// HOW DA FUQ DOES IT DO THAT???
+// Status: Works
 SELECT
   TrainRoutes.TrainNumber, CONCAT(DepartureStop.DepartureTime," - ",
     ArrivalStop.ArrivalTime, "\n", TIMEDIFF(ArrivalStop.ArrivalTime,
@@ -72,94 +71,128 @@ WHERE DepartureStop.StationName = %s AND
   TIMEDIFF(ArrivalStop.ArrivalTime, DepartureStop.DepartureTime) > "00:00:00"
   -- TIMEDIFF(ArrivalStop.ArrivalTime, DepartureStop.DepartureTime) <> "NULL"
 
-// NOT TESTED
-// Figure 8: Travel extras and passenger info
-Update ReservationDetails
-SET Baggage = %s, PassengerName = %s
-WHERE ReservationID = %s
+// Figure 8: Travel extras and passenger info - Number of Baggage dropdown
+// Status: Works
+SELECT SystemInfo.MaxBaggage
+FROM SystemInfo
 
 // Figure 8: Make reservation 1 use card dropdown
+// Status: Works
 SELECT CardNumber
 FROM PaymentInfo
 WHERE Username = %s
 
 // Figure 8: Make reservation 1 Student or nah
+// Status: Works
 SELECT Customers.Student
 FROM Customers
 WHERE Customers.Username = %s
 
-// Figure 8: Make reservation 1
--- SELECT
---   TrainRoutes.TrainNumber,
---   DepartureStop.DepartureTime,
---   ArrivalStop.ArrivalTime,
---   TIMEDIFF(ArrivalStop.ArrivalTime ,DepartureStop.DepartureTime) as Duration,
---   DepartureStop.StationName as DepartsFrom,
---   ArrivalStop.StationName as ArrivesAt,
---   CASE WHEN ReservationDetails.Class = "First Class" THEN TrainRoutes.FirstClassPrice as Price,
---        WHEN ReservationDetails.Class - "Second Class" Then TrainRoutes.SecondClassPrice as Price,
---   END CASE
---   ReservationDetails.Baggage as NumBaggages,
---   ReservationDetails.PassengerName
--- FROM TrainRoutes
--- INNER JOIN ReservationDetails ON ReservationDetails.TrainNumber = TrainRoutes.TrainNumber
--- INNER JOIN Stops as ArrivalStop ON TrainRoutes.TrainNumber = ArrivalStop.TrainNumber
--- INNER JOIN Stops as DepartureStop ON TrainRoutes.TrainNumber = DepartureStop.TrainNumber
--- WHERE DepartureStop.StationName = %s
---   AND ArrivalStop.StationName = %s
---   AND TIMEDIFF(ArrivalStop.ArrivalTime ,DepartureStop.DepartureTime) > "00:00:00"
-
 // Figure 10: Payment info Add card
+// Status: Works
 INSERT INTO PaymentInfo
 VALUES (%s, %s, %s, %s, Username)
 
-// Figure 10: Payment info Find Card to Delete dropdown
--- SELECT PaymentInfo.CardNummber
--- FROM PaymentInfo
--- INNER JOIN Customers ON PaymentInfo.Username = Customers.Username
--- WHERE Customers = %s
+// Figure 10: Does the customer already have this card in the database?
+// Status: Works
+SELECT *
+FROM PaymentInfo
+WHERE PaymentInfo.CardNumber = %s AND PaymentInfo.Username = %s
+
+// Figure 10: Payment info - Find Card to Delete dropdown
+// Status: Works
 SELECT CardNumber
 FROM PaymentInfo
 WHERE Username = %s
 
-
-// NOT TESTED
-// Figure 10: Payment info Delete Card
+// Figure 10: Payment info  Delete Card
+// Status: Nope
 DELETE PaymentInfo
 FROM PaymentInfo
 INNER JOIN Customers ON PaymentInfo.Username = Customers.Username
 INNER JOIN Reservations ON PaymentInfo.CardNummber = Reservations.CardNummber
 WHERE Customers = %s AND
   Reservations.Status = 0; --0 bit means the reservation was canceled
+-- DELETE PaymentInfo.*
+-- FROM PaymentInfo
+-- INNER JOIN Customers ON PaymentInfo.Username = Customers.Username
+-- INNER JOIN Reservations ON PaymentInfo.CardNumber = Reservations.CardNumber
+-- WHERE Customers.Username = "aliu3" AND
+--   Reservations.Status = 0
 
-DELETE PaymentInfo.*
-FROM PaymentInfo
-INNER JOIN Customers ON PaymentInfo.Username = Customers.Username
-INNER JOIN Reservations ON PaymentInfo.CardNumber = Reservations.CardNumber
-WHERE Customers.Username = "aliu3" AND
-  Reservations.Status = 0
+// Figure 11: Confirmation screen
+// Status: Works
+// Number of bags over max free
+SELECT %s - SystemInfo.FreeBaggage
+FROM SystemInfo
 
+// Figure 11: Confirmation screen
+// Status: Nope (SHOULD WORK WITH AUTOINCREMENT???)
+// Add to Reservations - IF CUSTOMER IS A STUDENT
+// Next ReservationID is generated using auto-increment
+// VALUES (%s, %s, 1, (PriceOfTicker + NumBagsOverTwo*30)*0.08)
+INSERT INTO Reservations(Username, CardNumber, Status, TotalCost)
+VALUES (%s, %s, 1, ((%s + %s*30)*0.08))
 
-// Pigure 11: Confirmation screen
-// Add to Reservations
-INSERT INTO Reservations
-VALUES (%s, %s, %s, %s, %s)
+// Figure 11: Confirmation screen
+// Status: Nope (SHOULD WORK WITH AUTOINCREMENT???)
+// Add to Reservations - IF CUSTOMER IS NOT A STUDENT
+// Next ReservationID is generated using auto-increment
+// VALUES (%s, %s, 1, (PriceOfTicker + NumBagsOverTwo*30))
+INSERT INTO Reservations(Username, CardNumber, Status, TotalCost)
+VALUES (%s, %s, 1, ((%s + %s*30)))
 
-// Pigure 11: Confirmation screen
+// Figure 11: Confirmation screen
+// Status: Works
 // Add to ReservationDetails
-INSERT INTO Customers
+INSERT INTO ReservationDetails (ReservationID, TrainNumber, PassengerName, Baggage, Class, DepartureDate, DepartsFrom, ArrivesAt)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 
 // Figure 13: Update reservation 2
-SELECT
+// Status: Nope
+// %s = ReservationID
+SELECT COUNT(*)
+FROM Reservations
+WHERE Reservations.ReservationID = %s
+
+// Figure 13: Update reservation 2
+// Status: Works
+// Get DepartsFrom in ReservationDetails; Used to generate table
+SELECT DepartsFrom
 FROM ReservationDetails
-INNER JOIN Reservations ON ReservationDetails.ReservationID = Reservations.ReservationID
-INNER JOIN TrainRoutes On ReservationDetails.TrainNumber = TrainRoutes.TrainNumber
-INNER JOIN Stops as ArrivalStop ON TrainRoutes.TrainNumber = ArrivalStop.TrainNumber
-INNER JOIN Stops as DepartureStop on TrainNumber.TrainNumber = DepartureStop.TrainNumber
-WHERE Reservations.ReservationID = %s AND
-  Reservations.Status = 1 AND
-  -- TIMEDIFF(ArrivalStop.ArrivalTime, DepartureStop.DepartureTime) > "00:00:00"
+WHERE ReservationID = %s
+
+// Figure 13: Update reservation 2
+// Status: Works
+// Get ArrivesAt in ReservationDetails
+SELECT ArrivesAt
+FROM ReservationDetails
+WHERE ReservationID = %s
+
+// Figure 13: Update reservation 2
+// Status: Works
+// Shows table of details of reservation; Used to generate table
+SELECT TrainRoutes.TrainNumber,
+  CONCAT(ReservationDetails.DepartureDate, " " , DepartureStop.DepartureTime," - ", ArrivalStop.ArrivalTime, "\n",
+    TIMEDIFF(ArrivalStop.ArrivalTime, DepartureStop.DepartureTime)) as Time,
+  CONCAT(DepartureStation.Location, "(", DepartureStation.StationName, ")") as DepartsFrom,
+  CONCAT(ArrivalStation.Location, "(", ArrivalStation.StationName, ")") as ArrivesAt,
+  TrainRoutes.FirstClassPrice,
+  TrainRoutes.SecondClassPrice,
+  ReservationDetails.Baggage,
+  ReservationDetails.PassengerName
+FROM TrainRoutes
+INNER JOIN ReservationDetails on TrainRoutes.TrainNumber = ReservationDetails.TrainNumber
+INNER JOIN Reservations on ReservationDetails.ReservationID = Reservations.ReservationID
+INNER JOIN Stops as ArrivalStop on TrainRoutes.TrainNumber = ArrivalStop.TrainNumber
+INNER JOIN Stops as DepartureStop on TrainRoutes.TrainNumber = DepartureStop.TrainNumber
+INNER JOIN Stations as ArrivalStation on ArrivalStop.StationName = ArrivalStation.StationName
+INNER JOIN Stations as DepartureStation on DepartureStop.StationName = DepartureStation.StationName
+WHERE ReservationDetails.ReservationID = %s
+  Reservations.Username = %s AND
+  DepartureStop.StationName = %s AND
+  ArrivalStop.StationName = %s AND
+  TIMEDIFF(ArrivalStop.ArrivalTime, DepartureStop.DepartureTime) > "00:00:00"
 
 // Figure 14: Update reservation 3
 
